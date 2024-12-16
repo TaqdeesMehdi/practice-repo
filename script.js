bd978462ce04d24302ad5e3b0000082fe39df2dc
Taqdees Mehdi <taqdeesm99@gmail.com>
2024-12-16 22:07:34 +0500

    changing

--- script.js
+++ script.js
@@ -1,29 +1,60 @@
-function arraymanipulate(array, instruction) {
-  const output = [];
-  for (let i = 0; i < array.length; i++) {
-    output.push(instruction(array[i]));
-  }
-  return output;
-}
-const divideby2 = (output) => {
-  return output > 2;
-};
-console.log(arraymanipulate([1, 2, 3, 4], divideby2));
+// function arraymanipulate(array, instruction) {
+//   const output = [];
+//   for (let i = 0; i < array.length; i++) {
+//     output.push(instruction(array[i]));
+//   }
+//   return output;
+// }
+// const divideby2 = (output) => {
+//   return output > 2;
+// };
+// console.log(arraymanipulate([1, 2, 3, 4], divideby2));
 
-function counter() {
-  let count = 0;
+// function counter() {
+//   let count = 0;
+//   function increment() {
+//     count++;
+
+//     console.log(count);
+//   }
+//   return increment;
+// }
+// const mycounter = counter();
+// mycounter();
+// mycounter();
+// mycounter();
+// const mycounter2 = counter();
+// mycounter2();
+// mycounter2();
+// mycounter2();
+// function f1(num) {
+//   let call = 0;
+//   function f2() {
+//     call++;
+//     console.log(call);
+//     return num * 2;
+//   }
+//   return f2;
+// }
+// const gen = f1(2);
+//  const result = gen(2);
+
+// const result2 = gen(3);
+
+// console.log(gen);
+// console.log(gen(2));
+// console.log(result);
+// console.log(result);
+function outer() {
+  let counter = 0;
   function increment() {
-    count++;
-
-    console.log(count);
+    counter++;
+    console.log(counter);
   }
-  return increment;
+  increment();
 }
-const mycounter = counter();
-mycounter();
-mycounter();
-mycounter();
-const mycounter2 = counter();
-mycounter2();
-mycounter2();
-mycounter2();
+outer();
+outer();
+outer();
+outer();
+outer();
