0b95a4c2da23fd53158d8e44d3532af58da21195
Taqdees Mehdi <taqdeesm99@gmail.com>
2024-12-24 21:19:07 +0500

    commit

--- script.js
+++ script.js
@@ -133,23 +133,23 @@
 // console.log(result([5, 6, 7, 8]));
 // console.log(result([5, 6, 7, 8]));
 // console.log(result([5, 6, 7, 8]));
-function objreturn() {
-  let id = 1;
-  let name = "kabootar";
-  let counter = 0;
-  function inner(obj1) {
-    counter++;
+// function objreturn() {
+//   let id = 1;
+//   let name = "kabootar";
+//   let counter = 0;
+//   function inner(obj1) {
+//     counter++;
 
-    if (counter > 10) {
-      return undefined;
-    } else {
-      return { 1: (obj1.id = id), 2: (obj1.name = name) };
-    }
-  }
-  return inner;
-}
-const result = objreturn();
-for (let i = 0; i <= 17; i++) {
-  const run = result({});
-  console.log(run);
-}
+//     if (counter > 10) {
+//       return undefined;
+//     } else {
+//       return { 1: (obj1.id = id), 2: (obj1.name = name) };
+//     }
+//   }
+//   return inner;
+// }
+// const result = objreturn();
+// for (let i = 0; i <= 17; i++) {
+//   const run = result({});
+//   console.log(run);
+// }
