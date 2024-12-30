d642751d45cdea09b8e05e2408d38e56be59a368
Taqdees Mehdi <taqdeesm99@gmail.com>
2024-12-30 19:15:36 +0500

    new commit

--- script.js
+++ script.js
@@ -152,14 +152,88 @@
 //   const run = result({});
 //   console.log(run);
 // }
-let array = ["a", "b", "c", "d", "e"];
-// const name = true;
-// if (name === true) {
-//   console.log(array.splice(-1));
-// } else {
-//   console.log(array.slice(1, -2));
+// let array = ["a", "b", "c", "d", "e"];
+// // const name = true;
+// // if (name === true) {
+// //   console.log(array.splice(-1));
+// // } else {
+// //   console.log(array.slice(1, -2));
+// // }
+// //.reverse() and .splice mutate the original array
+// array.splice(-1);
+// console.log(array);
+// console.log("array");
+// function squareOfZeroes(matrix) {
+//   const infoMatrix = preComputeNumOfZeroes(matrix);
+//   const lastIdx = matrix.length - 1;
+//   return hasSquareOfZeroes(infoMatrix, 0, 0, lastIdx, lastIdx, {});
 // }
-//.reverse() and .splice mutate the original array
-array.splice(-1);
-console.log(array);
-console.log("array");
+// // r1 is the top row, c1 is the left column
+// // r2 is the bottom row, c2 is the right column
+// function hasSquareOfZeroes(infoMatrix, r1, c1, r2, c2, cache) {
+//   if (r1 >= r2 || c1 >= c2) return false;
+//   const key =
+//     r1.toString() +
+//     "-" +
+//     c1.toString() +
+//     "-" +
+//     r2.toString() +
+//     "-" +
+//     c2.toString();
+//   if (key in cache) return cache[key];
+//   cache[key] =
+//     isSquareOfZeroes(infoMatrix, r1, c1, r2, c2) ||
+//     hasSquareOfZeroes(infoMatrix, r1 + 1, c1 + 1, r2 - 1, c2 - 1, cache) ||
+//     hasSquareOfZeroes(infoMatrix, r1, c1 + 1, r2 - 1, c2, cache) ||
+//     hasSquareOfZeroes(infoMatrix, r1 + 1, c1, r2, c2 - 1, cache) ||
+//     hasSquareOfZeroes(infoMatrix, r1 + 1, c1 + 1, r2, c2, cache) ||
+//     hasSquareOfZeroes(infoMatrix, r1, c1, r2 - 1, c2 - 1, cache);
+//   return cache[key];
+// }
+// // r1 is the top row, c1 is the left column
+// // r2 is the bottom row, c2 is the right column
+// function isSquareOfZeroes(infoMatrix, r1, c1, r2, c2) {
+//   const squareLength = c2 - c1 + 1;
+//   const hasTopBorder = infoMatrix[r1][c1].numZeroesRight >= squareLength;
+//   const hasLeftBorder = infoMatrix[r1][c1].numZeroesBelow >= squareLength;
+//   const hasBottomBorder = infoMatrix[r2][c1].numZeroesRight >= squareLength;
+//   const hasRightBorder = infoMatrix[r1][c2].numZeroesBelow >= squareLength;
+//   return hasTopBorder && hasLeftBorder && hasBottomBorder && hasRightBorder;
+// }
+// function preComputeNumOfZeroes(matrix) {
+//   const infoMatrix = matrix.map((row) =>
+//     row.map((value) => {
+//       const numZeroes = value === 0 ? 1 : 0;
+//       return { numZeroesBelow: numZeroes, numZeroesRight: numZeroes };
+//     })
+//   );
+//   const lastIdx = matrix.length - 1;
+//   for (let row = lastIdx; row >= 0; row--) {
+//     for (let col = lastIdx; col >= 0; col--) {
+//       if (matrix[row][col] === 1) continue;
+//       if (row < lastIdx) {
+//         infoMatrix[row][col].numZeroesBelow +=
+//           infoMatrix[row + 1][col].numZeroesBelow;
+//       }
+//       if (col < lastIdx) {
+//         infoMatrix[row][col].numZeroesRight +=
+//           infoMatrix[row][col + 1].numZeroesRight;
+//       }
+//     }
+//   }
+//   return infoMatrix;
+// }
+// // exports.squareOfZeroes = squareOfZeroes;
+// squareOfZeroes([1, 2, 3]);
+let post = "";
+let jsinput = document.querySelector(".a");
+let jsdiv = document.querySelector(".b");
+async function handleinput() {
+  post = await jsinput.value;
+  jsdiv.textContent = post;
+}
+async function clickhandle() {
+  jsinput.value = "";
+}
+jsinput.oninput = handleinput;
+jsinput.onclick = clickhandle;
